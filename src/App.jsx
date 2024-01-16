df4d20b3637c2318b8e4fefda4dcac54fc98365d
Goizane <goizaneolaneta@gmail.com>
2024-01-16 03:59:29 +0100

    Refactor Pokemons component and add new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,58 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import { useEffect } from "react";
 
 const BASE_URL = "https://pokeapi.co/api/v2";
 
+const getPokemons = (limit, start) => {
+  return fetch(
+    `${BASE_URL}/pokemon?limit=${limit ?? 151}&offset=${start ?? 0}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //resuelve las promesas que vienen dentro de un array y devuelve una promesa
+      //cuando se resuelven todas las promesas del array se resuelve esa promesa
+      return Promise.all(
+        data.results.map((pokemon) => getPokemon(pokemon.url))
+      );
+    });
+};
+
+const getPokemon = (url) => {
+  return fetch(url).then((response) => response.json());
+};
+
 const Pokemons = ({ pokemons }) => {
   return (
-    <div className="">
-      {pokemons.map((pokemon) => (
-        <div className="my-2" key={pokemon.name}>
-          <Pokemon pokemon={pokemon} />
-        </div>
-      ))}
-    </div>
+    <ul className="grid grid-cols-3">
+      {Array.isArray(pokemons) &&
+        pokemons.map((pokemon) => (
+          <li key={pokemon.name}>
+            <Pokemon pokemon={pokemon} />
+          </li>
+        ))}
+    </ul>
   );
 };
 
 const Pokemon = ({ pokemon }) => {
+  const { name, sprites } = pokemon;
   return (
     <>
-      <p>{pokemon.name}</p>
+      <p>{name}</p>
+      <img src={sprites.front_default} alt="" />
     </>
   );
-};
-
-const getPokemons = (limit, start) => {
-  return fetch(`${BASE_URL}/pokemon?limit=${limit ?? 151}&offset=${start ?? 0}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.results);
-      return data.results;
-    });
 };
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    getPokemons().then((data) => setPokemons(data));
+    getPokemons(900).then((data) => setPokemons(data));
   }, []);
+
+  console.log({ pokemons });
 
   return (
     <div className="bg-dark text-white flex justify-center items-center">
